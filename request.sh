8d8ce381c119bacbdb354f8de25a9372e3f96dd9
Yohanes Debebe <yohanesdebebe318@gmail.com>
2025-04-05 23:29:07 +0300

    test

---
+++ request.sh
@@ -0,0 +1,15 @@
+# Signup for admin
+# curl -X POST -d "action=signup&role=admin&email=yopeman318@gmail.com&password=12345678" http://localhost:8080/yope_php/Ethiopian_Tutoring_Platform/Back_End/user.php 
+
+# Get Request
+curl -X GET http://localhost:8080/yope_php/Ethiopian_Tutoring_Platform/Back_End/user.php
+
+
+
+
+
+
+
+
+# mywtech2017@gmail.com
+# Getachew_Enemayehu_Debebe_2017
